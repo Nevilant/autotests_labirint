54b3e117397926c76a96cdf5c5b42ce79aeb295f
Nevilant <valentin94.94@mail.ru>
2023-11-06 20:52:12 +0300

    added filters

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
@@ -21,6 +23,11 @@ class ProductPage(Base):
     manga = "#header-genres > div > ul > li:nth-child(6) > ul > li:nth-child(7) > a"
     main_word = "h1[class='genre-name']"
     all_filters = "div.navisort-line-one.swiper-slide.swiper-slide-active span:nth-child(3) .navisort-item__content"
+    list_publ_house = "#section-search-form > div:nth-child(4) > div.bl-name.block-pubhouse-bl-name"
+    publ_house = "#section-search-form > div:nth-child(4) .b-search-e-list-item:nth-child(2) label"
+    list_cover = "#section-search-form > div:nth-child(6) .bl-name"
+    cover = "#section-search-form > div:nth-child(6) .inputs div:first-child"
+    button_show = ".show-goods > input.show-goods__button"
 
     # Getters
 
@@ -44,6 +51,26 @@ class ProductPage(Base):
         return WebDriverWait(self.driver, 15).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, self.all_filters)))
 
+    def get_list_publ_house(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.list_publ_house)))
+
+    def get_publ_house(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.publ_house)))
+
+    def get_list_cover(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.list_cover)))
+
+    def get_cover(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.cover)))
+
+    def get_button_show(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.button_show)))
+
     # Actions
 
     def hover_books_menu(self):
@@ -62,6 +89,31 @@ class ProductPage(Base):
         ActionChains(self.driver).move_to_element(self.get_all_filters()).click().perform()
         print('Filters is open')
 
+    def click_list_publ_house(self):
+        ActionChains(self.driver).move_to_element(self.get_list_publ_house()).click().perform()
+
+    def click_publ_house(self):
+        ActionChains(self.driver).move_to_element(self.get_publ_house()).click().perform()
+        print('Select publishing house')
+
+    def close_list_publ_house(self):
+        ActionChains(self.driver).move_to_element(self.get_list_publ_house()).click().perform()
+
+    def click_list_cover(self):
+        ActionChains(self.driver).move_to_element(self.get_list_cover()).click().perform()
+        print('Open filter of cover')
+
+    def click_cover(self):
+        ActionChains(self.driver).move_to_element(self.get_cover()).click().perform()
+        print('Select type cover')
+
+    def close_list_cover(self):
+        ActionChains(self.driver).move_to_element(self.get_list_cover()).click().perform()
+
+    def click_button_show(self):
+        ActionChains(self.driver).move_to_element(self.get_button_show()).click().perform()
+        print('Click button')
+        time.sleep(10)
     # Methods
 
     def select_products(self):
@@ -70,3 +122,10 @@ class ProductPage(Base):
         self.click_manga()
         self.assert_words(self.get_main_word(), 'Манга')
         self.click_all_filters()
+        self.click_list_publ_house()
+        self.click_publ_house()
+        self.close_list_publ_house()
+        self.click_list_cover()
+        self.click_cover()
+        self.close_list_cover()
+        self.click_button_show()
