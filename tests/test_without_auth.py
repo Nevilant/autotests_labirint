fb9c734c0aee665e30a51e043a02af0ebbf81b65
Nevilant <valentin94.94@mail.ru>
2023-11-06 18:42:31 +0300

    screenshots method added in test file

--- tests/test_without_auth.py
+++ tests/test_without_auth.py
@@ -3,6 +3,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 
+from base.base_class import Base
+
 from pages.main_page import MainPage
 from pages.product_page import ProductPage
 
@@ -18,6 +20,9 @@ def test_with_auth():
 
     pp = ProductPage(driver)
     pp.select_products()
+
+    screen = Base(driver)
+    screen.get_screenshot()
 
 
 time.sleep(5)
