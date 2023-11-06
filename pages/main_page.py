0aa01e8d20437b2a91e519031f818ca67c441ef6
Nevilant <valentin94.94@mail.ru>
2023-11-06 16:28:06 +0300

    add main_page.py

---
+++ pages/main_page.py
@@ -0,0 +1,33 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from base.base_class import Base
+
+
+class MainPage(Base):
+    url = 'https://www.labirint.ru/'
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    # Locators
+
+    main_word = '[class="autodiscounts-content"] h2 a'
+
+    # Getters
+
+    def get_main_word(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.main_word)))
+
+    # Actions
+
+    # Methods
+
+    def open_website(self):
+        self.driver.get(self.url)
+        self.driver.maximize_window()
+        self.get_current_url()
+        self.assert_words(self.get_main_word(), 'Лучшая покупка дня')
