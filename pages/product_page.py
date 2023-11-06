f6c5f8a690902b02b455e02ea0c90ab749ac8dea
Nevilant <valentin94.94@mail.ru>
2023-11-06 18:18:22 +0300

    second part of dropdown menu opened added

--- pages/product_page.py
+++ pages/product_page.py
@@ -19,6 +19,7 @@ class ProductPage(Base):
     # Locators
 
     books_menu = "span.b-header-b-menu-e-link.top-link-menu.first-child"
+    comics_mangas_artbooks = "#header-genres > div > ul > li:nth-child(6) > span"
 
     # Getters
 
@@ -26,13 +27,22 @@ class ProductPage(Base):
         return WebDriverWait(self.driver, 15).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, self.books_menu)))
 
+    def get_comics_mangas_artbooks(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.comics_mangas_artbooks)))
+
     # Actions
 
     def hover_books_menu(self):
         ActionChains(self.driver).move_to_element(self.get_books_menu()).perform()
         print('Dropdown menu is open')
 
+    def hover_comics_mangas_artbooks(self):
+        ActionChains(self.driver).move_to_element(self.get_comics_mangas_artbooks()).perform()
+        print('Dropdown menu is open')
+
     # Methods
 
     def select_products(self):
         self.hover_books_menu()
+        self.hover_comics_mangas_artbooks()
