5406a2bf655cc3ad092e623ed45e9b4b316be1e4
Nevilant <valentin94.94@mail.ru>
2023-11-06 18:43:03 +0300

    the transition to the manga page has been added

--- pages/product_page.py
+++ pages/product_page.py
@@ -19,7 +19,8 @@ class ProductPage(Base):
     # Locators
 
     books_menu = "span.b-header-b-menu-e-link.top-link-menu.first-child"
-    comics_mangas_artbooks = "#header-genres > div > ul > li:nth-child(6) > span"
+    comics_manga_artbooks = "#header-genres > div > ul > li:nth-child(6) > span"
+    manga = "#header-genres > div > ul > li:nth-child(6) > ul > li:nth-child(7) > a"
 
     # Getters
 
@@ -27,9 +28,14 @@ class ProductPage(Base):
         return WebDriverWait(self.driver, 15).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, self.books_menu)))
 
-    def get_comics_mangas_artbooks(self):
+    def get_comics_manga_artbooks(self):
         return WebDriverWait(self.driver, 15).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, self.comics_mangas_artbooks)))
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.comics_manga_artbooks)))
+
+    def get_manga(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.manga)))
+
 
     # Actions
 
@@ -37,12 +43,18 @@ class ProductPage(Base):
         ActionChains(self.driver).move_to_element(self.get_books_menu()).perform()
         print('Dropdown menu is open')
 
-    def hover_comics_mangas_artbooks(self):
-        ActionChains(self.driver).move_to_element(self.get_comics_mangas_artbooks()).perform()
+    def hover_comics_manga_artbooks(self):
+        ActionChains(self.driver).move_to_element(self.get_comics_manga_artbooks()).perform()
         print('Dropdown menu is open')
+
+    def click_manga(self):
+        ActionChains(self.driver).move_to_element(self.get_manga()).click().perform()
+        print('Page is open')
 
     # Methods
 
     def select_products(self):
         self.hover_books_menu()
-        self.hover_comics_mangas_artbooks()
+        self.hover_comics_manga_artbooks()
+        self.click_manga()
+
