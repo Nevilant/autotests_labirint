d17710628032292782bb6fd72b065aedfc636c4b
Nevilant <valentin94.94@mail.ru>
2023-11-08 22:35:36 +0300

    add method select_product(adding products in cart) in product_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,19 +9,22 @@ from base.base_class import Base
 
 
 class ProductPage(Base):
-    """Прописываем локаторы нужнных нам элементов чтобы перейти в выбранный раздел
-        и добавляем выбранный товар в корзину """
+    """Выбираем нужную категорию товаров и сами товары"""
 
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
 
-    # Locators
+    # Locators for Categories
 
     books_menu = "span.b-header-b-menu-e-link.top-link-menu.first-child"
     comics_manga_artbooks = "#header-genres > div > ul > li:nth-child(6) > span"
     manga = "#header-genres > div > ul > li:nth-child(6) > ul > li:nth-child(7) > a"
     main_word = "h1[class='genre-name']"
+
+    # Locators for Products
+
+    button_add_in_cart_product_1 = "[id='buy961816']"
 
     # Getters
 
@@ -41,19 +44,27 @@ class ProductPage(Base):
         return WebDriverWait(self.driver, 15).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, self.main_word)))
 
+    def get_button_add_in_cart_product_1(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.button_add_in_cart_product_1)))
+
     # Actions
 
     def hover_books_menu(self):
         ActionChains(self.driver).move_to_element(self.get_books_menu()).perform()
-        print('Dropdown menu is open')
+        print('First dropdown menu is open')
 
     def hover_comics_manga_artbooks(self):
         ActionChains(self.driver).move_to_element(self.get_comics_manga_artbooks()).perform()
-        print('Dropdown menu is open')
+        print('Second dropdown menu is open')
 
     def click_manga(self):
         ActionChains(self.driver).move_to_element(self.get_manga()).click().perform()
         print('Page is open')
+
+    def click_button_add_in_cart_product_1(self):
+        ActionChains(self.driver).move_to_element(self.get_button_add_in_cart_product_1()).click().perform()
+        print('Product added in cart')
 
     # Methods
 
@@ -64,4 +75,4 @@ class ProductPage(Base):
         self.assert_words(self.get_main_word(), 'Манга')
 
     def select_product(self):
-        pass
+        self.click_button_add_in_cart_product_1()
