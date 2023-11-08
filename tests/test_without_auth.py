c60d99680f423ed46a8ebc41fa8bf02d40c65767
Nevilant <valentin94.94@mail.ru>
2023-11-08 23:39:42 +0300

    added 2 tests

--- tests/test_without_auth.py
+++ tests/test_without_auth.py
@@ -8,9 +8,10 @@ from base.base_class import Base
 from pages.main_page import MainPage
 from pages.product_page import ProductPage
 from pages.filters_page import FiltersPage
+from pages.cart_page import CartPage
 
 
-def test_with_auth():
+def test_without_auth_1():
     service = ChromeService(executable_path='/home/nevi/Documents/utilities/chromedriver-linux64/chromedriver')
     driver = webdriver.Chrome(service=service)
 
@@ -27,8 +28,56 @@ def test_with_auth():
 
     pp.select_product()
 
+    cp = CartPage(driver)
+    cp.use_cart()
+
     screen = Base(driver)
     screen.get_screenshot()
 
 
-time.sleep(5)
+def test_without_auth_2():
+    service = ChromeService(executable_path='/home/nevi/Documents/utilities/chromedriver-linux64/chromedriver')
+    driver = webdriver.Chrome(service=service)
+
+    print('Start test')
+
+    mp = MainPage(driver)
+    mp.open_website()
+
+    pp = ProductPage(driver)
+    pp.select_category()
+
+    fp = FiltersPage(driver)
+    fp.filters_fields()
+
+    pp.select_product()
+
+    cp = CartPage(driver)
+    cp.use_checkout()
+
+    screen = Base(driver)
+    screen.get_screenshot()
+
+
+def test_without_auth_3():
+    service = ChromeService(executable_path='/home/nevi/Documents/utilities/chromedriver-linux64/chromedriver')
+    driver = webdriver.Chrome(service=service)
+
+    print('Start test')
+
+    mp = MainPage(driver)
+    mp.open_website()
+
+    pp = ProductPage(driver)
+    pp.select_category()
+
+    fp = FiltersPage(driver)
+    fp.filters_fields()
+
+    pp.select_product()
+
+    cp = CartPage(driver)
+    cp.use_bubble_checkout()
+
+    screen = Base(driver)
+    screen.get_screenshot()
