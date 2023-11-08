c211a75006516a0e3f918e50458d828df5e73eff
Nevilant <valentin94.94@mail.ru>
2023-11-08 23:05:35 +0300

    deleted module time

--- pages/filters_page.py
+++ pages/filters_page.py
@@ -1,5 +1,3 @@
-import time
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
