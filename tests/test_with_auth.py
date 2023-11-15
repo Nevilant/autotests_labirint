2fd10ce54311bdb5612752e88c7156ac43ffff1f
Nevilant <valentin94.94@mail.ru>
2023-11-16 00:04:00 +0300

    module for test 2 and 3 is changed

--- tests/test_with_auth.py
+++ tests/test_with_auth.py
@@ -12,8 +12,8 @@ from pages.filters_page import FiltersPage
 from pages.cart_page import CartPage
 
 
-# @allure.description("Test with authorization 1")
-def test_with_auth_1():
+@allure.description("Test with authorization 1")
+def test_with_auth_1(set_up, set_group):
     service = ChromeService(executable_path='/home/nevi/Documents/utilities/chromedriver-linux64/chromedriver')
     driver = webdriver.Chrome(service=service)
 
@@ -39,51 +39,51 @@ def test_with_auth_1():
     driver.quit()
 
 
-# @allure.description("Test with authorization 2")
-# def test_without_auth_2(set_up):
-#     service = ChromeService(executable_path='/home/nevi/Documents/utilities/chromedriver-linux64/chromedriver')
-#     driver = webdriver.Chrome(service=service)
-#
-#     mp = MainPage(driver)
-#     mp.open_website()
-#
-#     pp = ProductPage(driver)
-#     pp.select_category()
-#
-#     fp = FiltersPage(driver)
-#     fp.filters_fields()
-#
-#     pp.select_product()
-#
-#     cp = CartPage(driver)
-#     cp.use_checkout()
-#
-#     screen = Base(driver)
-#     screen.get_screenshot()
-#
-#     driver.quit()
-#
-#
-# @allure.description("Test with authorization 3")
-# def test_without_auth_3(set_up):
-#     service = ChromeService(executable_path='/home/nevi/Documents/utilities/chromedriver-linux64/chromedriver')
-#     driver = webdriver.Chrome(service=service)
-#
-#     mp = MainPage(driver)
-#     mp.open_website()
-#
-#     pp = ProductPage(driver)
-#     pp.select_category()
-#
-#     fp = FiltersPage(driver)
-#     fp.filters_fields()
-#
-#     pp.select_product()
-#
-#     cp = CartPage(driver)
-#     cp.use_bubble_checkout()
-#
-#     screen = Base(driver)
-#     screen.get_screenshot()
-#
-#     driver.quit()
+@allure.description("Test with authorization 2")
+def test_without_auth_2(set_up):
+    service = ChromeService(executable_path='/home/nevi/Documents/utilities/chromedriver-linux64/chromedriver')
+    driver = webdriver.Chrome(service=service)
+
+    mp = MainPage(driver)
+    mp.open_website()
+
+    pp = ProductPageWithAuth(driver)
+    pp.select_category()
+
+    fp = FiltersPage(driver)
+    fp.filters_fields()
+
+    pp.select_product()
+
+    cp = CartPage(driver)
+    cp.use_checkout()
+
+    screen = Base(driver)
+    screen.get_screenshot()
+
+    driver.quit()
+
+
+@allure.description("Test with authorization 3")
+def test_without_auth_3(set_up):
+    service = ChromeService(executable_path='/home/nevi/Documents/utilities/chromedriver-linux64/chromedriver')
+    driver = webdriver.Chrome(service=service)
+
+    mp = MainPage(driver)
+    mp.open_website()
+
+    pp = ProductPageWithAuth(driver)
+    pp.select_category()
+
+    fp = FiltersPage(driver)
+    fp.filters_fields()
+
+    pp.select_product()
+
+    cp = CartPage(driver)
+    cp.use_bubble_checkout()
+
+    screen = Base(driver)
+    screen.get_screenshot()
+
+    driver.quit()
