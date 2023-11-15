a1b6eefc984a5ea5d7d3feca576a4a77bf03ab50
Nevilant <valentin94.94@mail.ru>
2023-11-15 23:29:46 +0300

    added class ProductPageWithAuth

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,10 +1,8 @@
-import time
-
 import allure
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 from base.base_class import Base
 from utilities.logger import Logger
@@ -23,9 +21,6 @@ class ProductPage(Base):
     comics_manga_artbooks = "#header-genres > div > ul > li:nth-child(6) > span"
     manga = "#header-genres > div > ul > li:nth-child(6) > ul > li:nth-child(7) > a"
     main_word = "h1[class='genre-name']"
-
-    # Locators for Products
-
     button_add_in_cart_product_1 = "[id='buy961816']"
 
     # Getters
@@ -71,6 +66,77 @@ class ProductPage(Base):
         with allure.step("Select category"):
             Logger.add_start_step(method="select_category")
             self.hover_books_menu()
+            self.hover_comics_manga_artbooks()
+            self.click_manga()
+            self.assert_words(self.get_main_word(), 'Манга')
+            Logger.add_end_step(url=self.driver.current_url, method="select_category")
+
+    def select_product(self):
+        with allure.step("Select product"):
+            Logger.add_start_step(method="select_product")
+            self.click_button_add_in_cart_product_1()
+            Logger.add_end_step(url=self.driver.current_url, method="select_product")
+
+
+class ProductPageWithAuth(Base):
+    """Выбираем нужную категорию товаров и сами товары"""
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    # Locators for Categories
+
+    books_menu_auth = "//*[@id='minwidth']/div[4]/div/div[1]/div[4]/div/div[1]/ul/li[1]/span"
+    comics_manga_artbooks = "#header-genres > div > ul > li:nth-child(6) > span"
+    manga = "#header-genres > div > ul > li:nth-child(6) > ul > li:nth-child(7) > a"
+    main_word = "h1[class='genre-name']"
+    button_add_in_cart_product_1 = "[id='buy961816']"
+
+    # Getters
+
+    def get_books_menu_auth(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.XPATH, self.books_menu_auth)))
+
+    def get_comics_manga_artbooks(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.comics_manga_artbooks)))
+
+    def get_manga(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.manga)))
+
+    def get_main_word(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.main_word)))
+
+    def get_button_add_in_cart_product_1(self):
+        return WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.button_add_in_cart_product_1)))
+
+    # Actions
+
+    def hover_books_menu_auth(self):
+        ActionChains(self.driver).move_to_element(self.get_books_menu_auth()).click().perform()
+        print("Hover books menu")
+
+    def hover_comics_manga_artbooks(self):
+        ActionChains(self.driver).move_to_element(self.get_comics_manga_artbooks()).perform()
+
+    def click_manga(self):
+        ActionChains(self.driver).move_to_element(self.get_manga()).click().perform()
+
+    def click_button_add_in_cart_product_1(self):
+        ActionChains(self.driver).move_to_element(self.get_button_add_in_cart_product_1()).click().perform()
+        print('Product added in cart')
+
+    # Methods
+
+    def select_category(self):
+        with allure.step("Select category"):
+            Logger.add_start_step(method="select_category")
+            self.hover_books_menu_auth()
             self.hover_comics_manga_artbooks()
             self.click_manga()
             self.assert_words(self.get_main_word(), 'Манга')
